d6d7f13ced29cf95073dbc9db6562fe057d56a90
raihan_parl <raihan.parl4@gmail.com>
2024-04-25 00:25:11 +0700

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://raihanparl.github.io',
-  base:"personal-site",
+  base:"",
   integrations: [mdx(), sitemap(), tailwind()]
 });
