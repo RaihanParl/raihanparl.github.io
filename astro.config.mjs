1ff5a9047f4a94306bc5eb215c6b14dcca3489a1
raihan_parl <raihan.parl4@gmail.com>
2024-04-25 01:11:06 +0700

    try redeploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://raihanparl.com/',
+  site: 'https://raihanparl.com',
   integrations: [mdx(), sitemap(), tailwind()]
 });
